9ed237411dbe20f1e5e2e1945e3d2f7a38256e3b
Caroline <carolineignacior@gmail.com>
2019-09-14 16:22:41 -0300

    Config node modules and create app.js to configure express

--- src/libraryManager.js
+++ src/libraryManager.js
@@ -1,42 +1,4 @@
-const options = {
-    props: [
-        { name: "editionYear", order: "asc" },
-        { name: "author", order: "asc" },
-        { name: "number", order: "desc" }
-    ],
-    books: [
-        {
-            number: 2,
-            title: "Patterns of Enterprise Application Architecture",
-            author: "Martin Fowler",
-            editionYear: 2002
-        },
-        {
-            number: 3,
-            title: "Head First Design Patterns",
-            author: "Elisabeth Freeman",
-            editionYear: 2004
-        },
-        {
-            number: 5,
-            title: "Harry Potter",
-            author: "Deitel & Deitel",
-            editionYear: 2007
-        },
-        {
-            number: 1,
-            title: "Java How To Program",
-            author: "Deitel & Deitel",
-            editionYear: 2007
-        },
-        {
-            number: 4,
-            title: "Internet & World Wide Web: How to Program",
-            author: "Felipe Neto",
-            editionYear: 2007
-        }
-    ]
-};
+const options;
 
 function evaluateElements(a, b, prop, order) {
 
