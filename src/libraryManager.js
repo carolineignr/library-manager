64e32731ababaaaf0b3c2faafd380061b3cb0246
Caroline <carolineignacior@gmail.com>
2019-09-14 16:54:14 -0300

    Add config.js to define sort settings and create readJSON function in libraryManager.js file to read data from config.js

--- src/libraryManager.js
+++ src/libraryManager.js
@@ -1,27 +1,44 @@
-const options;
+function readJSON() {
+    const fs = require('fs');
+
+    fs.readFile('./config.json', 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("Error reading file from disk:", err)
+            return
+        }
+        try {
+            const options = JSON.parse(jsonString);
+            ordenateCollection(options)
+        }
+        catch (err) {
+            console.log('Error parsing JSON string:', err)
+        }
+    })
+}
+
 
 function evaluateElements(a, b, prop, order) {
 
     if (order == "asc") {
-        if (a[prop] > b[prop]) {return 1};
-        if (a[prop] < b[prop]) {return -1};
+        if (a[prop] > b[prop]) { return 1 };
+        if (a[prop] < b[prop]) { return -1 };
     }
     if (order == "desc") {
-        if (a[prop] > b[prop]) {return -1};
-        if (a[prop] < b[prop]) {return 1};
+        if (a[prop] > b[prop]) { return -1 };
+        if (a[prop] < b[prop]) { return 1 };
     }
 
     return 0;
 }
 
-function ordenateCollection() {
+function ordenateCollection(options) {
 
     options.books.sort((a, b) => {
         let counter = 0;
         let result = 0;
 
         while (result == 0 && counter < options.props.length) {
-            
+
             const prop = [options.props[counter].name];
             const order = [options.props[counter].order];
 
@@ -29,12 +46,11 @@ function ordenateCollection() {
 
             counter++;
         }
-        
+
         return result;
     });
 
     console.log(options.books)
 }
 
-
-ordenateCollection();
+readJSON();
