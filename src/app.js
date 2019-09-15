eef2528f6c1ee53b3b432aed61defcb16fddd0c7
Caroline <carolineignacior@gmail.com>
2019-09-15 19:50:19 -0300

    Configre ejs to render index.ejs

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const getReturn = require("./libraryManager")
+const path = require("path");
+const getData = require("./libraryManager");
 
-app.set('views', path.join(__dirname, 'views'));    
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "views")));
+//app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
 
-app.get('/',function(req,res){
-    const result = getReturn("./config.json")
-    res.render('index', result)
+app.get("/", function(req, res) {
+  const result = getData("./config.json");
+  res.render("index", { data: result });
 });
-
 
 app.listen(process.env.port || 3000);
 
-console.log('Running at localhost port 3000');
+console.log("Running at localhost port 3000");
--- src/libraryManager.js
+++ src/libraryManager.js
@@ -1,72 +1,77 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function readJSON(path) {
-    return require(path)
+  return require(path);
 }
 
 function evaluateElements(a, b, prop, order) {
-
-    if (order == "asc") {
-        if (a[prop] > b[prop]) { return 1 };
-        if (a[prop] < b[prop]) { return -1 };
+  if (order == "asc") {
+    if (a[prop] > b[prop]) {
+      return 1;
     }
-    if (order == "desc") {
-        if (a[prop] > b[prop]) { return -1 };
-        if (a[prop] < b[prop]) { return 1 };
+    if (a[prop] < b[prop]) {
+      return -1;
     }
+  }
+  if (order == "desc") {
+    if (a[prop] > b[prop]) {
+      return -1;
+    }
+    if (a[prop] < b[prop]) {
+      return 1;
+    }
+  }
 
-    return 0;
+  return 0;
 }
 
-function ordenateCollection(options) {     
+function ordenateCollection(options) {
+  options.books.sort((a, b) => {
+    let counter = 0;
+    let result = 0;
 
-    options.books.sort((a, b) => {
-        let counter = 0;
-        let result = 0;
+    while (result == 0 && counter < options.props.length) {
+      const prop = options.props[counter].name;
+      const order = options.props[counter].order;
 
-        while (result == 0 && counter < options.props.length) {
+      result = evaluateElements(a, b, prop, order);
 
-            const prop = options.props[counter].name;
-            const order = options.props[counter].order;
+      counter++;
+    }
 
-            result = evaluateElements(a, b, prop, order);
+    return result;
+  });
 
-            counter++;
-        };
-
-        return result;
-    });
-
-    return options.books;
+  return options.books;
 }
 
 function validateOptions(option) {
-    return option.props.every(prop => {
-        if (prop.order == "") {
-            return false;
-        } 
-        if (!prop.order) {
-            throw new Error("Valor de ordenação: null");
-        }
-        return true;
-    });
+  if (!option.props.length) {
+    return false;
+  }
+
+  return option.props.every(prop => {
+    if (prop.order == "") {
+      return false;
+    }
+    if (!prop.order) {
+      throw new Error("Valor de ordenação: null");
+    }
+    return true;
+  });
 }
 
 function getBooks(path) {
-    try {
-
-        const options = readJSON(path)    
-        if (validateOptions(options)) {
-            return { data: ordenateCollection(options) } 
-        } 
-
-        return { error: "Valor de ordenação vazio"  }        
-    
-    } catch (err) {
-        return { error: err  }
-        // console.error(err);
-        // process.exit(0);
+  try {
+    const options = readJSON(path);
+    if (validateOptions(options)) {
+      return { data: ordenateCollection(options) };
     }
+
+    return [];
+  } catch (err) {
+    return { error: err };
+  }
 }
 
-module.exports = getBooks
+module.exports = getBooks;
